b5cacf2796b4c70a25085a9afcd3a35ec18b286f
sdfgbsujgboajgb <dirkjently45@gmail.com>
2025-10-05 06:25:52 +0400

    Update verify.js

--- api/verify.js
+++ api/verify.js
@@ -1,4 +1,8 @@
 export default function handler(request, response) {
+  // Устанавливаем правильные заголовки для Roblox
+  response.setHeader('Content-Type', 'application/json');
+  response.setHeader('Access-Control-Allow-Origin', '*');
+  
   response.status(200).json({
     "V2_Authentication": "success"
   });
