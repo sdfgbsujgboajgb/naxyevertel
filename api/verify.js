d5859172b36ac93f1347642ca033ad58a9758738
sdfgbsujgboajgb <dirkjently45@gmail.com>
2025-10-05 06:46:23 +0400

    Update verify.js

--- api/verify.js
+++ api/verify.js
@@ -1,10 +1,8 @@
 export default function handler(req, res) {
-  // Разрешаем CORS для Roblox
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
   
-  // Всегда возвращаем success
   res.status(200).json({
     V2_Authentication: "success"
   });
