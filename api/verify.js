86eab589b1d3c69a9eb9dcaa082c240ae1039757
sdfgbsujgboajgb <dirkjently45@gmail.com>
2025-10-05 06:00:44 +0400

    Update verify.js

--- api/verify.js
+++ api/verify.js
@@ -1,18 +1,3 @@
-// server.js (Node.js + Express)
-import express from "express";
-const app = express();
-
-app.use(express.json());
-
-// имитация эндпоинта для Pelinda
-app.post("/validate", (req, res) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-  
-  res.status(200).json({
-    status: "validated!!"
-  });
-});
-
-app.listen(3000, () => console.log("✅ Pelinda mock server running on port 3000"));
+export default function handler(req, res) {
+  res.status(200).json({ "V2_Authentication": "success" });
+}
